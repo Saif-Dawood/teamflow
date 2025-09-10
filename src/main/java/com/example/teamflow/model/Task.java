53e19e29d7a5d8004b0a2dee0c662caff44db6ed
Saif-Dawood <saifdawoodcs@gmail.com>
2025-09-10 14:54:46 +0300

    [UPDT] Task Class: DEL lombok.Data, ADD getters/setters

--- src/main/java/com/example/teamflow/model/Task.java
+++ src/main/java/com/example/teamflow/model/Task.java
@@ -8,10 +8,7 @@ import com.example.teamflow.enums.Priority;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 
-import lombok.Data;
-
 @Entity
-@Data
 public class Task {
     
     @Id
@@ -36,11 +33,19 @@ public class Task {
     }
 
     public void update(Task task) {
-        this.title = task.title;
-        this.description = task.description;
-        this.status = task.status;
-        this.dueDate = task.dueDate;
-        this.priority = task.priority;
+
+        if (task.getTitle() != null) {
+            this.title = task.title;
+        } if (task.getDescription() != null) {
+            this.description = task.description;
+        } if (task.getStatus() != null) {
+            this.status = task.status;
+        } if (task.getDueDate() != null) {
+            this.dueDate = task.dueDate;
+        } if (task.getPriority() != null) {
+            this.priority = task.priority;
+        }
+
     }
 
     @Override
@@ -51,4 +56,52 @@ public class Task {
             ", dueDate: " + this.dueDate +
             ", priority: " + this.priority;
     }
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public TaskStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(TaskStatus status) {
+		this.status = status;
+	}
+
+	public LocalDateTime getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(LocalDateTime dueDate) {
+		this.dueDate = dueDate;
+	}
+
+	public Priority getPriority() {
+		return priority;
+	}
+
+	public void setPriority(Priority priority) {
+		this.priority = priority;
+	}
 }
